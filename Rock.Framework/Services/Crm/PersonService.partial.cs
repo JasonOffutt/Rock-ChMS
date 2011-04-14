b909fab22f3e9533e376d9b4322fd71398c002e9
JasonOffutt <jason@jordanrift.com>
2011-04-14 12:21:59 -0700

    Adding partial to PersonService. Removing partial from ignore.

---
+++ Rock.Framework/Services/Crm/PersonService.partial.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Principal;
+using Rock.Helpers;
+using Rock.Models.Crm;
+
+namespace Rock.Services.Crm
+{
+    public partial class PersonService
+    {
+        public bool IsAllowedTo(OperationType operationType, Person person, IPrincipal user)
+        {
+            // TODO: Hook into Authorization functionality
+            return true;
+        }
+    }
+}
