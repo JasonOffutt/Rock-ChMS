dfbc2e07e5e705ae28f107501504cf17ee3711a1
JasonOffutt <jason@jordanrift.com>
2011-04-14 12:14:06 -0700

    Creating helper abstraction to get membership user from model.

---
+++ Rock.Framework/Helpers/MembershipHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Security;
+
+namespace Rock.Helpers
+{
+    public static class MembershipHelper
+    {
+        public static MembershipUser GetMembershipUserFromModel(string providerName, Rock.Models.Cms.User user)
+        {
+            DateTime now = DateTime.Now;
+
+            return new MembershipUser(
+                providerName,
+                user.Username,
+                user.PersonId,
+                user.Email,
+                user.PasswordQuestion,
+                user.Comment,
+                user.IsApproved ?? true,
+                user.IsLockedOut ?? false,
+                user.CreationDate ?? now,
+                user.LastLoginDate ?? now,
+                user.LastActivityDate ?? now,
+                user.LastPasswordChangedDate ?? now,
+                user.LastLockedOutDate ?? now);
+        }
+    }
+}
