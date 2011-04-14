885cf64b4c4df0d1964bb417600a49a1f05580df
JasonOffutt <jason@jordanrift.com>
2011-04-14 12:20:20 -0700

    Adding class to house constants.

---
+++ Rock.Framework/Helpers/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rock.Helpers
+{
+    public enum OperationType
+    {
+        View,
+        Edit,
+        EditSecurity
+    }
+
+    public static class Constants
+    {
+    }
+}
